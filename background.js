e6e2347f95293ffaa1587991706fcd5ada234433
Martin Liberg <martin.liberg@drivekitt.com>
2019-01-12 17:50:30 +0100

    Alter request and response headers if the url of the request matches

---
+++ background.js
@@ -0,0 +1,27 @@
+const handler = createRequestHandler({
+  url: "google.com",
+  requestHeaders: {
+    Origin: {
+      from: "localhost",
+      to: "google.com"
+    }
+  },
+  responseHeaders: {
+    "access-control-allow-origin": {
+      from: "google.com",
+      to: "localhost"
+    }
+  }
+})
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  handler,
+  { urls: ["<all_urls>"] },
+  ["blocking", "requestHeaders"]
+)
+
+chrome.webRequest.onHeadersReceived.addListener(
+  handler,
+  { urls: ["<all_urls>"] },
+  ["blocking", "responseHeaders"]
+)
